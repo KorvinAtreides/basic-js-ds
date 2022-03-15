aa36a803794aafc93e2dd6b859e1158b3cb89f9a
KorvinAtreides <Korvin1312b@yandex.by>
2022-03-15 15:36:04 +0300

    feat: add binary tree class

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,39 +6,92 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.tree = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.tree;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (!this.tree) {
+      this.tree = new Node(data);
+      return;
+    }
+    let copySubTree = this.tree;
+    while (true) {
+      if (copySubTree.data > data) {
+        if (!copySubTree.left) {
+          copySubTree.left = new Node(data);
+          return;
+        }
+        copySubTree = copySubTree.left;
+      } else {
+        if (!copySubTree.right) {
+          copySubTree.right = new Node(data);
+          return;
+        }
+        copySubTree = copySubTree.right;
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return !!this.find(data);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let copySubTree = this.tree;
+    while (true) {
+      if (!copySubTree || copySubTree.previousData == data) return null;
+      if (copySubTree.data == data) return copySubTree;
+      if (copySubTree.data) {
+        copySubTree = copySubTree.data > data ? copySubTree.left : copySubTree.right;
+      } else {
+        copySubTree = copySubTree.previousData > data ? copySubTree.left : copySubTree.right;
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    if (!this.find(data)) return null;
+    let copySubTree = this.tree;
+    if (copySubTree.data == data) {
+      copySubTree.data == undefined;
+      copySubTree.previousData = data;
+      return null;
+    }
+    while (true) {
+      if (copySubTree.left && copySubTree.left.data == data){
+        copySubTree.left.data = undefined;
+        copySubTree.left.previousData = data;
+        return null;
+      } else if (copySubTree.right && copySubTree.right.data == data){
+        copySubTree.right.data = undefined;
+        copySubTree.right.previousData = data;
+        return null;
+      }
+      copySubTree = copySubTree.data > data ? copySubTree.left : copySubTree.right;
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.tree) return null;
+    let copySubTree = this.tree;
+    while (true) {
+      if (!copySubTree.left || (!copySubTree.left.data &&!copySubTree.left.left)) return copySubTree.data;
+      copySubTree = copySubTree.left;
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.tree) return null;
+    let copySubTree = this.tree;
+    while (true) {
+      if (!copySubTree.right|| (!copySubTree.right.data &&!copySubTree.right.right)) return copySubTree.data;
+      copySubTree = copySubTree.right;
+    }
   }
 }
 
